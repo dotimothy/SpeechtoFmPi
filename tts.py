7c078c71c043796488ee5d058edfd37700da872c
Timothy Do <timothydobsa@gmail.com>
2020-10-06 17:37:16 -0700

    Update tts.py

--- tts.py
+++ tts.py
@@ -2,24 +2,33 @@
 
 import os
 print("****Installing Libraries/Dependencies****\n")
-os.system("sudo apt-get install sox")
-os.system("sudo pip install gtts")
 print()
+os.system("sudo apt-get install ffmpeg")
+print("\nFinished installing ffmpeg\n")
+os.system("sudo pip install pydub")
+print("\nFinished installing pydub\n")
+os.system("sudo pip install gtts")
+print("\nFinished installing gtts\n")
 os.system("sudo make")
+print("\nFinished making executable for fm_transmittewr\n")
 print("\n****Finish Installing Dependencies****\n")
 
 from gtts import gTTS
-import wave
+from pydub import AudioSegment
 
 
 def main():
 	txt = raw_input("\nType what the text, it will be speech: ")
 	gt = gTTS(txt)
-	gt.save("output.wav")
+	src = "output.mp3"
+	dst = "output.wav"
+	gt.save(src)
+	sound = AudioSegment.from_mp3(src)
+	sound.export(dst,format="wav")
 	print("Output has been compiled to output.wav.")
 	freq = float(input("Specify the frequency you would like to broadcast: "))
 	print("Broadcasting your output at " + str(freq) + " mHZ!")
-	os.system("sox output.wav -r 44100 -c 1 -b 16 -t wav - | sudo ./fm -f " + str(float(freq)) + " output.wav")
+	os.system("sudo ./fm -f " + str(float(freq)) + " output.wav")
 	print("Your output has been compiled and broadcasted.")
 	os.system("sudo rm output.wav")
 	os.system("sudo rm output.mp3")
