d939fc403b9c740f905964bd24329f4d9429eb43
Timothy Do <timothydobsa@gmail.com>
2020-10-06 17:11:45 -0700

    updated lets see if this works

--- tts.py
+++ tts.py
@@ -1,10 +1,8 @@
 #Author Timothy Do
 
-import os 
-#Author Timothy Do
-
 import os
 print("****Installing Libraries/Dependencies****\n")
+os.system("sudo apt-get install sox lixsox-fmt-mp3")
 os.system("sudo pip install gtts")
 print()
 os.system("sudo make")
@@ -17,13 +15,15 @@ import wave
 def main():
 	txt = raw_input("\nType what the text, it will be speech: ")
 	gt = gTTS(txt)
-	gt.save("output.wav")
+	gt.save("output.mp3")
+	os.system("sox output.mp3 -r 44100 -c 1 -b 16 -t wav output.wav")
 	print("Output has been compiled to output.wav.")
 	freq = float(input("Specify the frequency you would like to broadcast: "))
 	print("Broadcasting your output at " + str(freq) + " mHZ!")
 	os.system("sudo ./fm -f " + str(float(freq)) + " output.wav")
 	print("Your output has been compiled and broadcasted.")
-	#os.system("sudo rm output.wav")
+	os.system("sudo rm output.wav")
+	os.system("sudo rm output.mp3")
 	exit()
 
 main()
